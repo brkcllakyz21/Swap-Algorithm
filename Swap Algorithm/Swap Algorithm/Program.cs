c89c6a8f29eb8499a360271a420cc44c1f75d0aa
brkcllakyz21 <37548000+brkcllakyz21@users.noreply.github.com>
2018-10-29 20:55:14 +0300

    Add files via upload

---
+++ Swap Algorithm/Swap Algorithm/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Elinizde bulunan bir dizide kullanıcının istediği 2 elemanın yerini değiştirebilecek algoritmayı tasarlayınız.
+
+            int[] dizi = new int[10];
+            int c = 3;
+            int ilk = 0, son = 0, swap = 0;
+            Console.Write("Dizi = ");
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                dizi[i] = c;
+                Console.Write(dizi[i] + "-");
+                c += 3;
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Yerlerinin değiştirilmesini istediğiniz sayılardan ilkinin indisini giriniz = ");
+            ilk = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Yerlerinin değiştirilmesini istediğiniz sayılardan ikincisinin indisini giriniz = ");
+            son = Convert.ToInt32(Console.ReadLine());
+            swap = dizi[son];
+            dizi[son] = dizi[ilk];
+            dizi[ilk] = swap;
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                Console.Write(dizi[i] + "-");
+            }
+
+        }
+    }
+}
